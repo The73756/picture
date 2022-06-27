5dcdc46bdb27e136791f3c7ebc56ab689202820d
The73756 <the73756the@gmail.com>
2022-06-27 14:39:17 +0900

    add simple sliders

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,8 @@
 import modals from './modules/modals';
+import sliders from './modules/sliders';
 
 window.addEventListener('DOMContentLoaded', () => {
 	modals();
+	sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+	sliders('.main-slider-item', 'vertical');
 });
---
+++ src/js/modules/sliders.js
@@ -0,0 +1,70 @@
+const sliders = (slides, dir, prev, next) => {
+	let slideIndex = 1,
+		paused = false;
+	const items = document.querySelectorAll(slides);
+
+	function showSlides(n) {  
+		if (n > items.length) {
+			slideIndex = 1;
+		}
+		
+		if (n < 1) {
+			slideIndex = items.length;
+		}
+
+		items.forEach(el => {
+			el.classList.add('animated');
+			el.style.display = 'none';
+		});
+
+		items[slideIndex - 1].style.display = 'block';
+	}
+
+	showSlides(slideIndex);
+
+	function plusSlides(n) {  
+		showSlides(slideIndex += n);
+	}
+
+	try {
+		const	prevBtn = document.querySelector(prev),
+			nextBtn = document.querySelector(next);
+		
+		prevBtn.addEventListener('click', () => {
+			plusSlides(-1);
+			items[slideIndex -1].classList.add('fadeIn');
+		});	
+
+		nextBtn.addEventListener('click', () => {
+			plusSlides(1);
+			items[slideIndex -1].classList.add('fadeIn');
+		});	
+
+	} catch(e) {}
+
+	function activateAnimation() {  
+		if (dir === 'vertical') {
+			paused = setInterval(function() {
+				plusSlides(1);
+				items[slideIndex -1].classList.add('slideInDown');
+			}, 5000);
+		} else {
+			paused = setInterval(function() {
+				plusSlides(1);
+				items[slideIndex -1].classList.add('fadeIn');
+			}, 5000);
+		}
+	}
+
+	activateAnimation();
+
+	items[0].parentNode.addEventListener('mouseenter', () => {
+		clearInterval(paused);
+	});
+
+	items[0].parentNode.addEventListener('mouseleave', () => {
+		activateAnimation();
+	});
+};
+
+export default sliders;
